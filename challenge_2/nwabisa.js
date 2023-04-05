c3c7871e83c21aef7f8034c39f3dba31727232b8
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-04-05 08:53:38 +0200

    Placed variables inside of object and exported object

---
+++ challenge_2/nwabisa.js
@@ -0,0 +1,11 @@
+export const nwabisa = {
+  firstname: "Nwabisa",
+  surname: "Gabe",
+  role: "CEO",
+};
+
+const display =
+  nwabisa.firstname + " " + nwabisa.surname + " (" + nwabisa.role + ")";
+document.querySelector("#nwabisa").innerText = display;
+
+export default nwabisa;
